4110fda7955c4c8e63d01b1e7cf943494b7104e8
Chris Harrington <ironiridis@gmail.com>
2021-11-04 23:45:26 -0500

    Add fields for HTTP request type

--- apigw/httpReq.go
+++ apigw/httpReq.go
@@ -1,12 +1,15 @@
 package apigw
 
 type HTTPReq struct {
-	Headers map[string]string `json:"headers"`
-	Query   map[string]string `json:"queryStringParameters"`
-	Req     struct {
+	Headers        map[string]string `json:"headers"`
+	Query          map[string]string `json:"queryStringParameters"`
+	RouteKey       string            `json:"routeKey"`
+	PathParameters map[string]string `json:"pathParameters"`
+	Req            struct {
 		HTTP struct {
-			Method string `json:"method"`
-			Path   string `json:"path"`
+			Method   string `json:"method"`
+			Path     string `json:"path"`
+			SourceIP string `json:"sourceIp"`
 		} `json:"http"`
 	} `json:"requestContext"`
 	Body string `json:"body"`
